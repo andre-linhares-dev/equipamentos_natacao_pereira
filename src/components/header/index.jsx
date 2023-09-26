c7e1dd8aeb63e73486e6d8e6ec211fe5766d24fe
André Linhares Dev <de.lin@hotmail.com>
2023-09-26 17:59:45 -0300

    Create a responsive navbar

---
+++ src/components/header/index.jsx
@@ -0,0 +1,22 @@
+import './styles.css';
+import { ShoppingBagIcon } from '@heroicons/react/24/solid'
+
+const Header = () => {
+    return (
+    <nav className="navBar">
+        <img src="/src/assets/img/Acquashop-logo.png" alt="logo" className='max-w-[15vw]'/>
+        <ul className='flex items-center'>
+            <li className='px-4'><a href="/">Home</a></li>
+            <li className='px-4'><a href="/">Produtos</a></li>
+            <li className='px-4'><a href="/">Suporte</a></li>
+            <li className='px-4'><a href="/">Contato</a></li>
+            <ShoppingBagIcon className="h-7 w-7 text-cyan-950 mx-7"/>
+            <input type="search" name="search" id="searchbox" placeholder='Pesquisa' className='rounded-lg m-2 ps-4 max-w-[12vw] border-sky-900 border-2 h-8'/>
+            
+        </ul>
+    </nav>
+    )
+}
+
+export { Header }
+
